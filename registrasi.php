56dcf577c4a6a229c7053d2c81abe9aab9a5f37a
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-10-01 10:31:09 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Form Registrasi</title>
+	<title>registrasi</title>
 </head>
 <body>
-
-
-
+	<form action="prosesregis.php" method="post">
+		Nim : <input type="text" name="nim"><br> <br>
+		Nama : <input type="text" name="nama"><br><br>
+		Email : <input type="email" name="email"><br><br>
+		<input type="submit" name="submit" value="submit">
+	</form>
 </body>
 </html>
