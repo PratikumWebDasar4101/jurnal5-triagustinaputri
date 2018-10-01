69155cf6f76dedc17473b473892ff2f63b5f7e5f
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-10-01 10:38:25 +0700

    Add files via upload

---
+++ hasilkomen.php
@@ -0,0 +1,10 @@
+<?php
+$komen =$_POST["komen"];
+if(strlen($komen) < 5){
+	echo "KOMENTAR >5!";
+	echo "<br><a href='komentar.php'>RE-Comment</a>";
+}else{
+	echo $komen . "<br><br>";
+	echo "panjang komentar : " . strlen($komen);
+}
+?>
